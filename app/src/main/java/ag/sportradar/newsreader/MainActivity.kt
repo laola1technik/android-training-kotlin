7069a4df5238b16cf737c58c9aa6e06d156bfaf8
Christian Frank <christian.frank.84@gmail.com>
2019-03-07 14:21:02 +0100

    Shows NewsData response.

--- app/src/main/java/ag/sportradar/newsreader/MainActivity.kt
+++ app/src/main/java/ag/sportradar/newsreader/MainActivity.kt
@@ -1,12 +1,27 @@
 package ag.sportradar.newsreader
 
+import ag.sportradar.newsreader.api.NewsApiClient
+import ag.sportradar.newsreader.api.NewsData
+import ag.sportradar.newsreader.api.OnNewsDataFetchedListener
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnNewsDataFetchedListener {
+
+    override fun onNewsDataFetched(data: NewsData) {
+        responseData.text = data.status
+    }
+
+    override fun onFailure(message: String) {
+        responseData.text = message
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsApiClient = NewsApiClient(getString(R.string.news_api_key))
+        newsApiClient.getTopHeadlines(this)
     }
 }
--- app/src/main/java/ag/sportradar/newsreader/api/NewsApiClient.kt
+++ app/src/main/java/ag/sportradar/newsreader/api/NewsApiClient.kt
@@ -4,13 +4,15 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 class NewsApiClient(val apiKey: String) {
 
     val retrofit = Retrofit.Builder()
-            .baseUrl("https://newsapi.org/v2/")
-            .build()
+        .baseUrl("https://newsapi.org/v2/")
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
 
     val newsService: NewsService = retrofit.create(NewsService::class.java)
 
